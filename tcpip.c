bfbef9a9db9ec1d393f2d981c3ba9916303f3dae
Chris West (Faux) <git@goeswhere.com>
2015-11-23 21:34:12 +0000

    whitespace

--- tcpip.c
+++ tcpip.c
@@ -10,7 +10,7 @@
 
 #define tcp_assert assert
 static const uint8_t IP_PROTOCOL_TCP = 6;
-static const uint8_t IP_FLAG_DONT_FRAGMENT  = 1 << 6;
+static const uint8_t IP_FLAG_DONT_FRAGMENT = 1 << 6;
 static const uint8_t IP_FLAG_MORE_FRAGMENTS = 1 << 5;
 
 static const uint8_t TCP_FLAG_ACK = 1 << 4;
@@ -26,8 +26,8 @@ static uint8_t top_4_bits(char byte) {
 }
 
 static uint16_t read_uint16_t(const char *buf) {
-    return (uint8_t)buf[0] * 0x100
-         + (uint8_t)buf[1];
+    return (uint8_t) buf[0] * 0x100
+           + (uint8_t) buf[1];
 }
 
 static void write_uint32_t(char *buf, uint16_t val) {
@@ -44,10 +44,10 @@ static void write_uint16_t(char *buf, uint16_t val) {
 }
 
 static uint32_t read_uint32_t(const char *buf) {
-    return (uint8_t)buf[0] * 0x1000000
-         + (uint8_t)buf[1] * 0x10000
-         + (uint8_t)buf[2] * 0x100
-         + (uint8_t)buf[3];
+    return (uint8_t) buf[0] * 0x1000000
+           + (uint8_t) buf[1] * 0x10000
+           + (uint8_t) buf[2] * 0x100
+           + (uint8_t) buf[3];
 }
 
 enum conn_state {
@@ -99,8 +99,8 @@ static void insert_waiting_port(struct tcb *tcb, struct port_data *port) {
 
 void tcp_fd_set(struct tcb *tcb, fd_set *rd_set, fd_set *wr_set) {
     for (struct waiting_port *curr = tcb->waiting;
-            NULL != curr;
-            curr = curr->next) {
+         NULL != curr;
+         curr = curr->next) {
         // TODO read or write or whatever?
         FD_SET(curr->value->upstream, rd_set);
         FD_SET(curr->value->upstream, wr_set);
@@ -109,8 +109,8 @@ void tcp_fd_set(struct tcb *tcb, fd_set *rd_set, fd_set *wr_set) {
 
 void tcp_fd_consume(struct tcb *tcb, fd_set *rd_set, fd_set *wr_set) {
     for (struct waiting_port *curr = tcb->waiting;
-            NULL != curr;
-            curr = curr->next) {
+         NULL != curr;
+         curr = curr->next) {
         int writable = FD_ISSET(curr->value->upstream, wr_set);
         if (writable && CONN_SYN_RECIEVED == curr->value->conn_state) {
             // TODO connection accepted, time to ACK the client
@@ -118,29 +118,29 @@ void tcp_fd_consume(struct tcb *tcb, fd_set *rd_set, fd_set *wr_set) {
             ack[0] = 0x45;
             ack[2] = 0;
             ack[3] = 40;
-            write_uint16_t(ack+4, rand());
+            write_uint16_t(ack + 4, rand());
             ack[6] = IP_FLAG_DONT_FRAGMENT;
-            ack[8] = (char)0xff;
+            ack[8] = (char) 0xff;
             ack[9] = IP_PROTOCOL_TCP;
 
-            memcpy(ack+12, curr->value->source, 4);
-            memcpy(ack+16, curr->value->dest, 4);
+            memcpy(ack + 12, curr->value->source, 4);
+            memcpy(ack + 16, curr->value->dest, 4);
 
-            write_uint16_t(ack+20, curr->value->source_port);
-            write_uint16_t(ack+22, curr->value->dest_port);
-            write_uint32_t(ack+24, ++curr->value->sequence_out);
-            write_uint32_t(ack+28, 0); // TODO acking
+            write_uint16_t(ack + 20, curr->value->source_port);
+            write_uint16_t(ack + 22, curr->value->dest_port);
+            write_uint32_t(ack + 24, ++curr->value->sequence_out);
+            write_uint32_t(ack + 28, 0); // TODO acking
             ack[32] = 0x50; // no options
             ack[33] = TCP_FLAG_SYN | TCP_FLAG_ACK;
-            write_uint16_t(ack+34, 1460);
+            write_uint16_t(ack + 34, 1460);
         }
     }
 }
 
 void tcp_consume(struct tcb *tcb, const char *buf, size_t len) {
-    tcp_assert(top_4_bits(buf[0]) == 4);
+            tcp_assert(top_4_bits(buf[0]) == 4);
     const uint16_t ip_header_length = (buf[0] & 0x0f) * 32 / 8;
-    tcp_assert(ip_header_length == 20);
+            tcp_assert(ip_header_length == 20);
     assert(len > 20);
 
     // buf[1]: DSCP / ECN: unsupported
@@ -148,7 +148,7 @@ void tcp_consume(struct tcb *tcb, const char *buf, size_t len) {
     const uint16_t identifcation = read_uint16_t(buf + 4);
 
     // buf[6:7]: flags / fragmentation
-    tcp_assert(buf[6] & IP_FLAG_DONT_FRAGMENT);
+            tcp_assert(buf[6] & IP_FLAG_DONT_FRAGMENT);
 
     // buf[8]: ttl (ignored)
     const uint8_t protocol = (uint8_t) buf[9];
@@ -179,15 +179,15 @@ void tcp_consume(struct tcb *tcb, const char *buf, size_t len) {
     struct port_data *const port = &tcb->ports[source_port];
 
     if (main_flags & TCP_FLAG_SYN) {
-        tcp_assert(CONN_LISTENING == port->conn_state);
+                tcp_assert(CONN_LISTENING == port->conn_state);
         port->upstream = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
         assert(port->upstream > 0);
 
         struct sockaddr_in addr;
         memset(&addr, 0, sizeof(struct sockaddr_in));
         if (connect(port->upstream,
-                (struct sockaddr*)&addr,
-                sizeof(struct sockaddr_in))) {
+                    (struct sockaddr *) &addr,
+                    sizeof(struct sockaddr_in))) {
             assert(EINPROGRESS == errno);
         }
 
